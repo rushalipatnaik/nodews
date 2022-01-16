20bad6e928288e2d290c87b6b1880a5fd97cb70d
Rushali <rushalipatnaik18@gmail.com>
2022-01-16 18:46:52 +0530

    update

--- basichttpsserver/index.js
+++ basichttpsserver/index.js
@@ -3,17 +3,29 @@ const port = 8000;
 const fs = require('fs');
 
 function requestHandler(req, res){
-  console.log(req.url);
+  // console.log(req.url);
   res.writeHead(200, {'content-type': 'text/html'});
 
-  fs.readFile('./index.html',function(err,data){
+  let filepath;
+
+  switch(req.url){
+    case '/': 
+            filepath="index.html";
+            break;
+    case '/profile':
+            filepath="profile.html";
+            break;
+    default:
+            filepath="404.html";
+  }
+
+  fs.readFile(filepath, function(err,data){
     if(err){
-      console.log('error',err);
-      return res.end('<h1>Error!</h1>');
+      return console.log("err:", err);
     }
     return res.end(data);
   })
-  // res.end('Hey Love!');
+
 }
 
 
